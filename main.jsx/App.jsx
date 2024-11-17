2bcd1159ab3a35165903258854f663a67215ad67
tamarabush <tamara.abush14@outlook.com>
2024-11-17 15:11:15 -0500

    Create ProductList component to render each ProductItem

--- main.jsx/App.jsx
+++ main.jsx/App.jsx
@@ -1,3 +1,5 @@
+//TASK 2 - Create the App Component:
+
 import React, { useState } from "react";
 import ProductList from "./ProductList";
 import AddProductForm from "./AddProductForm";
@@ -7,7 +9,7 @@ const App = () => {
     { id: 12479, name: "Perfume", price: "$80", description: "8oz Vanilla Spray bottle perfum." },
     { id: 22479, name: "Mascara", price: "$25", description: "Fine black mascara tube with roll brush." },
     { id: 22479, name: "Blush", price: "$20", description: "Deep red blush tube." },
-    { id: 22479, name: "Powder", price: "$20", description: "Fine matte face powder." },
+    { id: 22479, name: "Powder", price: "$20", description: "Fine matte face." },
   ]);
 
   const addProduct = (newProduct) => {
---
+++ main.jsx/ProductList.jsx
@@ -0,0 +1,19 @@
+//TASK 3 - Create the ProductList Component:
+
+import React from "react";
+import ProductItem from "./ProductItem";
+
+const ProductList = ({ products }) => {
+  return (
+    <div>
+      <h2>Product List</h2>
+      <ul>
+        {products.map((product) => (
+          <ProductItem key={product.id} product={product} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProductList;
