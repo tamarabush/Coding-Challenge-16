8b673ad3ca160a5c5fcba512e9f2032c8b2c5d1e
tamarabush <tamara.abush14@outlook.com>
2024-11-17 15:09:27 -0500

    Set up App component with initial product list

---
+++ main.jsx/App.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import ProductList from "./ProductList";
+import AddProductForm from "./AddProductForm";
+
+const App = () => {
+  const [products, setProducts] = useState([
+    { id: 12479, name: "Perfume", price: "$80", description: "8oz Vanilla Spray bottle perfum." },
+    { id: 22479, name: "Mascara", price: "$25", description: "Fine black mascara tube with roll brush." },
+    { id: 22479, name: "Blush", price: "$20", description: "Deep red blush tube." },
+    { id: 22479, name: "Powder", price: "$20", description: "Fine matte face powder." },
+  ]);
+
+  const addProduct = (newProduct) => {
+    setProducts((prevProducts) => [...prevProducts, newProduct]);
+  };
+
+  return (
+    <div>
+      <h1>Product Dashboard</h1>
+      <AddProductForm addProduct={addProduct} />
+      <ProductList products={products} />
+    </div>
+  );
+};
+
+export default App;
